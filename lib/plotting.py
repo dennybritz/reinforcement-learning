518a514333f622bbfa652c0678be5aa61b5544fa
Denny Britz <dennybritz@gmail.com>
2016-09-17 11:04:35 -0700

    Deep Q Updates

--- lib/plotting.py
+++ lib/plotting.py
@@ -60,28 +60,39 @@ def plot_value_function(V, title="Value Function"):
 
 
 
-def plot_episode_stats(stats, smoothing_window=10):
+def plot_episode_stats(stats, smoothing_window=10, noshow=False):
     # Plot the episode length over time
-    plt.figure(figsize=(10,5))
+    fig1 = plt.figure(figsize=(10,5))
     plt.plot(stats.episode_lengths)
     plt.xlabel("Epsiode")
     plt.ylabel("Epsiode Length")
     plt.title("Episode Length over Time")
-    plt.show()
+    if noshow:
+        plt.close(fig1)
+    else:
+        plt.show(fig1)
 
     # Plot the episode reward over time
-    plt.figure(figsize=(10,5))
+    fig2 = plt.figure(figsize=(10,5))
     rewards_smoothed = pd.Series(stats.episode_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
     plt.plot(rewards_smoothed)
     plt.xlabel("Epsiode")
     plt.ylabel("Epsiode Reward (Smoothed)")
     plt.title("Episode Reward over Time (Smoothed over window size {})".format(smoothing_window))
-    plt.show()
+    if noshow:
+        plt.close(fig2)
+    else:
+        plt.show(fig2)
 
     # Plot time steps and episode number
-    plt.figure(figsize=(10,5))
+    fig3 = plt.figure(figsize=(10,5))
     plt.plot(np.cumsum(stats.episode_lengths), np.arange(len(stats.episode_lengths)))
     plt.xlabel("Time Steps")
     plt.ylabel("Episode")
     plt.title("Episode per time step")
-    plt.show()
+    if noshow:
+        plt.close(fig3)
+    else:
+        plt.show(fig3)
+
+    return fig1, fig2, fig3
